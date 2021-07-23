5a023d380aa0784c24bd83c68fe6efcb638f84ac
zhanlutuzi <74134348+zhanlutuzi@users.noreply.github.com>
2021-07-23 10:04:16 +0800

    Update Animal.cpp

--- Animal.cpp
+++ Animal.cpp
@@ -8,6 +8,10 @@ class Animal
 public:
 	Animal() {};
 	Animal(const string &a);
+	string getname()
+	{
+		return m_strName;
+	}
 private:
 	string m_strName;
 };
@@ -20,24 +24,30 @@ class Reptile :virtual public Animal
 {
 public:
 	Reptile() {};
-	Reptile(const string&) { Animal(); }
-	void run() {};
+	Reptile(const string&b) :Animal(b) { ; }
+	void run() { cout << this->getname()<<" has run!\n"; };
 };
 class Aquatic :virtual public Animal
 {
 public:
 	Aquatic() {};
-	void run() {};
-	Aquatic(const string&) { Animal(); }
+	void run() { cout << this->getname() << " has run!\n"; };
+	Aquatic(const string&c):Animal(c) { ; }
 };
 
 class Fork :public Reptile, public Aquatic
 {
 public:
-	Fork(const string &) { Animal(); }
+	Fork(const string &d):Animal(d) { ; }
 };
 
 int main()
 {
+	Animal A1("Animal1");
+	Reptile R1("Reputile1");
+	R1.run();
+	Aquatic Aq1("Aquatic1");
+	Aq1.run();
+	Fork F1("Fork1");
 	return 0;
 }
